8c105cbb0dd0ea3c4417352daa3e5cfffb2e209a
Anakin Pregitzer <anakin@pregitzer.de>
2026-06-24 23:03:20 +0200

    pull OCI images and run via overlayfs

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module mini_container_tool
 
 go 1.24.4
+
+require github.com/4n4k1n/ociregistry v0.0.0-20260624204146-a81c7c7e35be // indirect
--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"strings"
 	"syscall"
+
+	"github.com/4n4k1n/ociregistry"
 )
+
+type spec struct {
+	Layers  []string
+	Cmd     []string
+	Env     []string
+	WorkDir string
+}
 
 func main() {
 	switch os.Args[1] {
@@ -20,45 +32,76 @@ func main() {
 
 func parent() {
 	fmt.Println("parent pid:", os.Getpid())
-	// re-exec
-	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 
-	// Adding namespaces
-	cmd.SysProcAttr = &syscall.SysProcAttr{
+	image := os.Args[2]
+	result, err := ociregistry.Pull(image, "latest", "/tmp/oci/"+image)
+	must(err)
+
+	cmd := append(result.Config.Entrypoint, result.Config.Cmd...)
+	if len(os.Args) > 3 {
+		cmd = os.Args[3:]
+	}
+
+	var layers []string
+	for _, l := range result.Layers {
+		layers = append(layers, l.Dir)
+	}
+
+	f, err := os.CreateTemp("", "container*.json")
+	must(err)
+	must(json.NewEncoder(f).Encode(spec{layers, cmd, result.Config.Env, result.Config.WorkingDir}))
+	f.Close()
+
+	c := exec.Command("/proc/self/exe", "child", f.Name())
+	c.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
 	}
-
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-
-	if err := cmd.Run(); err != nil {
-		fmt.Println("ERROR: ", err)
+	c.Stdin, c.Stdout, c.Stderr = os.Stdin, os.Stdout, os.Stderr
+	if err := c.Run(); err != nil {
+		fmt.Println("ERROR:", err)
 		os.Exit(1)
 	}
+	os.Remove(f.Name())
 }
 
 func child() {
 	fmt.Println("child pid:", os.Getpid())
 
+	f, err := os.Open(os.Args[2])
+	must(err)
+	var s spec
+	must(json.NewDecoder(f).Decode(&s))
+	f.Close()
+
+	upper, work, merged := "/tmp/overlay/upper", "/tmp/overlay/work", "/tmp/overlay/merged"
+	os.MkdirAll(upper, 0755)
+	os.MkdirAll(work, 0755)
+	os.MkdirAll(merged, 0755)
+
+	// lowerdir: top layer first
+	lowerdirs := make([]string, len(s.Layers))
+	for i, l := range s.Layers {
+		lowerdirs[len(s.Layers)-1-i] = l
+	}
+	opts := fmt.Sprintf("lowerdir=%s,upperdir=%s,workdir=%s", strings.Join(lowerdirs, ":"), upper, work)
+
 	must(syscall.Mount("", "/", "", syscall.MS_PRIVATE|syscall.MS_REC, ""))
-	must(syscall.Mount("rootfs", "rootfs", "", syscall.MS_BIND, ""))
-	must(os.MkdirAll("rootfs/oldrootfs", 0700))
-	must(syscall.PivotRoot("rootfs", "rootfs/oldrootfs"))
+	must(syscall.Mount("overlay", merged, "overlay", 0, opts))
+	must(os.MkdirAll(filepath.Join(merged, "oldrootfs"), 0700))
+	must(syscall.PivotRoot(merged, filepath.Join(merged, "oldrootfs")))
 	must(os.Chdir("/"))
 	must(syscall.Unmount("/oldrootfs", syscall.MNT_DETACH))
 	must(os.Remove("/oldrootfs"))
 	must(syscall.Mount("proc", "proc", "proc", 0, ""))
 
-	// execution in child process
-	cmd := exec.Command(os.Args[2], os.Args[3:]...)
-
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-
+	cmd := exec.Command(s.Cmd[0], s.Cmd[1:]...)
+	cmd.Stdin, cmd.Stdout, cmd.Stderr = os.Stdin, os.Stdout, os.Stderr
+	cmd.Env = s.Env
+	if s.WorkDir != "" {
+		cmd.Dir = s.WorkDir
+	}
 	if err := cmd.Run(); err != nil {
-		fmt.Println("ERROR: ", err)
+		fmt.Println("ERROR:", err)
 		os.Exit(1)
 	}
 }
